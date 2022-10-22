2ea22a586dd4670fdba90e3099df7d8a8fedc0cb
Drew Harris <dsharris10@gmail.com>
2022-10-22 17:57:17 -0500

    Create grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="bg-blue-400">Test</div>;
+  return (
+    <div className="grid grid-rows-[1.8fr_1fr] h-full absolute top-0 bottom-0 left-0 right-0 p-2 gap-2 grid-cols-[2fr_2fr_1.3fr]">
+      <div className="bg-mc-300 border-4 border-mc-800 col-span-2">test</div>
+      <div className="bg-mc-600  row-span-2">test2</div>
+      <div className="bg-mc-600 ">test3</div>
+      <div className="bg-mc-600 ">test4</div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mc: {
+          100: "#C5C6C6",
+          200: "#A9A9A9",
+          300: "#888888",
+          400: "#6F6E6F",
+          500: "#5D5D5D",
+          600: "#4B4B4B",
+          700: "#3B3B3B",
+          800: "#2A2A2A",
+          900: "#1C1C1C",
+        },
+      },
+    },
   },
   plugins: [],
 };
