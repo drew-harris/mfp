c768de58048a5244957ec22a410166361ff413b6
Drew Harris <harrisd@smu.edu>
2023-02-07 18:54:19 -0600

    Custom tutorial update comparison

--- src/components/tutorial/TutorialPanel.tsx
+++ src/components/tutorial/TutorialPanel.tsx
@@ -1,13 +1,33 @@
 import { useEffect } from "react";
-import { useNodeStore } from "../../stores/nodes";
+import { RFState, useNodeStore } from "../../stores/nodes";
 import { useTutorialStore } from "../../stores/tutorialStore";
+
+const customCompare = (oldState: RFState, newState: RFState): boolean => {
+  if (oldState.nodes.length !== newState.nodes.length) {
+    return false;
+  }
+  if (oldState.edges.length !== newState.edges.length) {
+    return false;
+  }
+  for (let i = 0; i < oldState.nodes.length; i++) {
+    if (oldState.nodes[i].id !== newState.nodes[i].id) {
+      return false;
+    }
+  }
+  for (let i = 0; i < oldState.edges.length; i++) {
+    if (oldState.edges[i].id !== newState.edges[i].id) {
+      return false;
+    }
+  }
+  return true;
+};
 
 export const TutorialPanel = () => {
   const currentStep = useTutorialStore((s) => s.currentStep);
   const nextStep = useTutorialStore((s) => s.nextStep);
 
   // TODO: Create a custom comparison function for the node store to reduce rerenders
-  const state = useNodeStore();
+  const state = useNodeStore((s) => s, customCompare);
 
   useEffect(() => {
     console.log("Checking");
