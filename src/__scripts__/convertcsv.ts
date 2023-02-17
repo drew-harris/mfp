3a1bc2b09f0014000a61fcd7502552a610d7798b
Drew Harris <harrisd@smu.edu>
2023-02-17 12:39:27 -0600

    Parse csv and create item id list

--- src/__scripts__/convertcsv.ts
+++ src/__scripts__/convertcsv.ts
@@ -1,3 +1,46 @@
-console.log("test");
+import { parseFile } from "fast-csv";
+
+interface Row {
+  source: string;
+  target: string;
+  weight: number;
+}
+
+async function parseCsv(): Promise<Row[]> {
+  return new Promise((resolve, reject) => {
+    const rows: Row[] = [];
+    parseFile("src/hardcoded/edge_sheet.csv", {
+      delimiter: ";",
+    })
+      .on("error", (error) => reject(error))
+      .on("data", (row: string[]) => {
+        rows.push({
+          source: row[0],
+          target: row[1],
+          weight: Number(row[2]),
+        });
+      })
+      .on("end", () => {
+        resolve(rows);
+      });
+  });
+}
+
+const rows = await parseCsv();
+
+const ids = new Set<string>();
+
+rows.forEach((row) => {
+  if (!row.source.startsWith("r_")) {
+    ids.add(row.source);
+  }
+
+  if (!row.target.startsWith("r_")) {
+    ids.add(row.target);
+  }
+});
+
+console.log(ids);
+console.log(ids.size);
 
 export {};
