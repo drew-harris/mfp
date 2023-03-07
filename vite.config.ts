241b698da9e57857d45383fcaa276a29e0c6b7e6
Drew Harris <harrisd@smu.edu>
2023-03-06 21:18:14 -0600

    Change vite build to use relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "",
 
   // eslint-disable-next-line
   // @ts-ignore
