5b08fc879108f8d7ceb6af971ff3159f6dfb08ea
Drew Harris <harrisd@smu.edu>
2023-04-07 19:38:29 -0500

    remove imports in <App />

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import FactoryPlanner from "./FactoryPlanner";
 import { LoginPage } from "./LoginPage";
 import { useUserStore } from "./stores/userStore";
-import { pullMFPData, pushMFPData } from "./utils/gqlqueries";
 
 export default function App() {
   const userId = useUserStore((s) => s.id);
