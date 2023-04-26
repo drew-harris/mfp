8d1ce123af9ba82bd9cbd9eb84e53dad707941ea
Drew Harris <harrisd@smu.edu>
2023-04-26 11:32:36 -0500

    Develop tutorial further

--- src/hardcoded/missions.ts
+++ src/hardcoded/missions.ts
@@ -1,3 +1,4 @@
+import { MCNodeType } from "../types/MCNodes";
 import { Mission } from "../types/tasks";
 
 export const allMissions: Mission[] = [
@@ -91,10 +92,18 @@ export const allMissions: Mission[] = [
         },
       },
       {
+        id: "tutorial-resource-desc",
+        title: "Resource Node",
+        description:
+          "This is a resource node, it represents an infinite supply of a basic item needed for crafting. You can adjust how many are being output by entering in an amount // MAKE EDITIBLE",
+        idPool: ["item.minecraft:diamond"],
+        continuation: true,
+      },
+      {
         id: "tutorial-delete",
         title: "Deleting nodes",
         description:
-          "Click on the resource node and use the backspace key to delete it (will update when i make button)",
+          "Click on the resource node and use the backspace key or the delete icon in the lower right corner to delete it",
         idPool: ["item.minecraft:diamond"],
         stateRequirement: (state) => {
           return !state.nodes.some(
@@ -106,8 +115,143 @@ export const allMissions: Mission[] = [
       },
       {
         id: "tutorial-recipe",
-        title: "Recipe Node",
-        description: "Pause here",
+        title: "Crafting Node",
+        description:
+          "Crafting nodes are a way to turn a resource into a new item with a crafting recipe. In this example, we will craft a furnace, which requires 9 cobblestone to build, drag the furnace crafting node to the canvas.",
+        idPool: ["minecraft:furnace"],
+        stateRequirement: (state) => {
+          return Boolean(
+            state.nodes.some((n) => n.data.dataType === MCNodeType.crafter)
+          );
+        },
+      },
+      {
+        id: "tutorial-recipe-multiple",
+        title: "Crafting Node",
+        description:
+          "Notice that you can use the arrows at the top of the crafter to select different recipes, for this tutorial we use recipe 1/3 which uses cobblestone",
+        idPool: ["minecraft:furnace"],
+        continuation: true,
+      },
+      {
+        id: "tutorial-recipe-bring-in",
+        title: "Crafting Node",
+        description: "Now, drag a cobblestone resource node to the canvas",
+        idPool: ["minecraft:furnace", "minecraft:cobblestone"],
+        stateRequirement(state) {
+          return Boolean(
+            state.nodes.some(
+              (n) =>
+                n.data.dataType === MCNodeType.resource &&
+                n.data.item.itemId === "minecraft:cobblestone"
+            )
+          );
+        },
+      },
+      {
+        id: "tutorial-recipe-connect",
+        title: "Crafting Node",
+        description:
+          "Connect the resource node to the crafter by dragging the circle to create a path. (reword probably)",
+        idPool: ["minecraft:furnace", "minecraft:cobblestone"],
+        stateRequirement(state) {
+          return Boolean(
+            state.edges.some(
+              (n) => (n.data.item.itemId = "minecraft:cobblestone")
+            )
+          );
+        },
+      },
+      {
+        id: "tutorial-recipe-connect",
+        title: "Crafting Node",
+        description:
+          "Try adjusting the amount of cobblestone in the resource node",
+        idPool: ["minecraft:furnace", "minecraft:cobblestone"],
+        continuation: true,
+      },
+      {
+        id: "tutorial-info-mode",
+        title: "Info mode",
+        description:
+          'In order to see the flow of items on the canvas, we need to use info mode. You can turn it on and off by using the "Info Mode" button at the top of the canvas',
+        idPool: ["minecraft:furnace", "minecraft:cobblestone"],
+        continuation: true,
+      },
+      {
+        id: "tutorial-info-mode",
+        title: "Info mode",
+        description:
+          'In order to see the flow of items on the canvas, we need to use info mode. You can turn it on and off by using the "Info Mode" button at the top of the canvas',
+        idPool: ["minecraft:furnace", "minecraft:cobblestone"],
+        continuation: true,
+      },
+      {
+        id: "tutorial-splitter-delete",
+        title: "Splitter Node",
+        description:
+          "We also need to craft some cobblestone slabs. First delete the path between the resource node and the crafter node. Select it with the mouse and use the delete button on the bottom right of the canvas.",
+        idPool: ["minecraft:furnace", "minecraft:cobblestone"],
+        stateRequirement(state) {
+          return state.edges.length === 0;
+        },
+      },
+      {
+        id: "tutorial-splitter-delete",
+        title: "Splitter Node",
+        description:
+          "Drag the splitter node to the canvas between the two other nodes.",
+        idPool: ["minecraft:furnace", "minecraft:cobblestone"],
+        stateRequirement(state) {
+          return Boolean(
+            state.nodes.some((n) => n.data.dataType === MCNodeType.splitter)
+          );
+        },
+      },
+      {
+        id: "tutorial-splitter-crafter",
+        title: "Splitter Node",
+        description: "Drag the cobblestone crafter to the canvas. ",
+        idPool: [
+          "minecraft:furnace",
+          "minecraft:cobblestone",
+          "minecraft:cobblestone_slab",
+        ],
+        stateRequirement(state) {
+          return Boolean(
+            state.nodes.some(
+              (n) =>
+                n.data.dataType === MCNodeType.crafter &&
+                n.data.item.itemId === "minecraft:cobblestone_slab"
+            )
+          );
+        },
+      },
+      {
+        id: "tutorial-splitter-connect",
+        title: "Splitter Node",
+        description:
+          "Connect the resouce node to the lefthand side inputs of the splitter node and connect the right side of the splitter nodes to the two inputs",
+        idPool: [
+          "minecraft:furnace",
+          "minecraft:cobblestone",
+          "minecraft:cobblestone_slab",
+        ],
+        stateRequirement(state) {
+          return state.edges.length === 3;
+        },
+      },
+      {
+        id: "tutorial-splitter-connect",
+        title: "Splitter Node",
+        description:
+          "You can use the slider on the splitter node to adjust the ratio of items that the outputs recieve.",
+        idPool: [
+          "minecraft:furnace",
+          "minecraft:cobblestone",
+          "minecraft:cobblestone_slab",
+        ],
+        continuation: true,
       },
     ],
   },
