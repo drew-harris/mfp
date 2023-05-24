9033de9e2d70f238293c3365b120d24559f056bb
Drew Harris <harrisd@smu.edu>
2023-05-24 12:43:43 -0500

    Remove tutorial comments

--- src/hardcoded/missions.ts
+++ src/hardcoded/missions.ts
@@ -94,7 +94,7 @@ export const allMissions: Mission[] = [
         id: "tutorial-resource-desc",
         title: "Resource Node",
         description:
-          "This is a resource node, it represents an infinite supply of a basic item needed for crafting. You can adjust how many are being output by entering in an amount // MAKE EDITIBLE",
+          "This is a resource node, it represents an infinite supply of a basic item needed for crafting. You can adjust how many are being output by entering in an amount",
         idPool: ["item.minecraft:diamond"],
         continuation: true,
       },
@@ -151,7 +151,7 @@ export const allMissions: Mission[] = [
         id: "tutorial-recipe-connect",
         title: "Crafting Node",
         description:
-          "Connect the resource node to the crafter by dragging the circle to create a path. (reword probably)",
+          "Connect the resource node to the crafter by dragging the circle to create a path.",
         idPool: ["minecraft:furnace", "minecraft:cobblestone"],
         stateRequirement(state) {
           return Boolean(
@@ -210,7 +210,7 @@ export const allMissions: Mission[] = [
       {
         id: "tutorial-splitter-crafter",
         title: "Splitter Node",
-        description: "Drag the cobblestone crafter to the canvas. ",
+        description: "Drag the cobblestone slab crafter to the canvas. ",
         idPool: [
           "minecraft:furnace",
           "minecraft:cobblestone",
@@ -244,7 +244,7 @@ export const allMissions: Mission[] = [
         id: "tutorial-splitter-connect",
         title: "Splitter Node",
         description:
-          "You can use the slider on the splitter node to adjust the ratio of items that the outputs recieve.",
+          'You can use the input on the splitter node to adjust the ratio of items that the outputs recieve. For example, "AAB" will allocate twice as many materials to the first output than the second',
         idPool: [
           "minecraft:furnace",
           "minecraft:cobblestone",
