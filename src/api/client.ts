aae1a6219484cb16d4968267d7945197bc8bd262
Drew Harris <harrisd@smu.edu>
2024-02-04 15:50:55 -0600

    Update lapis url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: import.meta.env["VITE_API_URL"] || "https://lapis.drewh.net/query",
+  uri:
+    import.meta.env["VITE_API_URL"] || "https://lapis.humingamelab.com/query",
   cache: new InMemoryCache(),
 });
